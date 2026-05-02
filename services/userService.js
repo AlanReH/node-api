7e70b3e50fb8ef0282337fd7e69d4aa71b5e0f71
AlanReH <alanrevilla@gmail.com>
2026-05-02 16:50:23 -0600

    set default users

--- services/userService.js
+++ services/userService.js
@@ -2,6 +2,42 @@ import { v4 as uuidv4 } from 'uuid';
 
 let users = [];
 
+users.push({
+  id: uuidv4(),
+  name: "Alan",
+  email: "alan@example.com",
+  phone: "+525512345678",
+  password: "1234",
+  tax_id: "AARR990101XXX",
+  created_at: new Date(),
+  addresses: [
+    {
+      street: "Calle Falsa 123",
+      city: "Ciudad de México",
+      country: "México"
+    }
+  ]
+});
+
+users.push({
+  id: uuidv4(),
+  name: "María",
+  email: "maria@example.com",
+  phone: "+525587654321",
+  password: "5678",
+  tax_id: "MARR990101XXX",
+  created_at: new Date(),
+  addresses: [
+    {
+      street: "Avenida Siempreviva 456",
+      city: "Guadalajara",
+      country: "México"
+    }
+  ]
+});
+
+
+
 const getUsers = (sortedBy, filter) => {
   let result = [...users];
 
