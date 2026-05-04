c975c751890e228a492591198a09f486026ad634
AlanReH <alanrevilla@gmail.com>
2026-05-03 18:09:02 -0600

    testing swagger change

--- app.js
+++ app.js
@@ -22,7 +22,9 @@ app.use(express.urlencoded({ extended: false }));
 // #region Routes
 app.use('/', indexRouter);
 app.use('/users', usersRouter);
+console.log(`loading swagger spec: ${JSON.stringify(swaggerSpec)}`);
 app.use('/api-docs', swaggerUi.serve, swaggerUi.setup(swaggerSpec));
+console.log(`Swagger UI available at http://localhost:${port}/api-docs`);
 
 // #region Errors
 
