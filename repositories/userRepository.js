eb36c2ebcc575c25e6af24a6111c30cac427e431
AlanReH <alanrevilla@gmail.com>
2026-05-05 22:20:12 -0600

    fix bad path of logger

--- repositories/userRepository.js
+++ repositories/userRepository.js
@@ -1,5 +1,5 @@
 import { pool } from '../config/db.js';
-import { logger } from '../config/logger.js';
+import { logger } from '../utils/logger.js';
 
 export const createUser = async (user) => {
   const client = await pool.connect();
