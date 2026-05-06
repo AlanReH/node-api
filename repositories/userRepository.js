0081a2359db3397e6cb3b54563513b34bda948c1
AlanReH <alanrevilla@gmail.com>
2026-05-05 22:23:20 -0600

    fix logger bad import

--- repositories/userRepository.js
+++ repositories/userRepository.js
@@ -1,5 +1,5 @@
 import { pool } from '../config/db.js';
-import { logger } from '../utils/logger.js';
+import logger from '../utils/logger.js';
 
 export const createUser = async (user) => {
   const client = await pool.connect();
